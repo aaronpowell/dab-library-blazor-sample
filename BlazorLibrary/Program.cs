1beee33df09509e43f8f2e07994643584b17f737
Aaron Powell <me@aaron-powell.com>
2023-04-06 20:59:56 +1000

    first draft of blazor app

--- BlazorLibrary/Program.cs
+++ BlazorLibrary/Program.cs
@@ -8,4 +8,8 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+builder.Services
+    .AddLibraryClient()
+    .ConfigureHttpClient(client => client.BaseAddress = new Uri("https://localhost:5001/graphql"));
+
 await builder.Build().RunAsync();
