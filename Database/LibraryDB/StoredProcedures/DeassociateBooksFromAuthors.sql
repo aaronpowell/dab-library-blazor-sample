7ee68ee88b8d52214563066432bca005edf3fac1
Davide Mauri <damauri@microsoft.com>
2023-04-10 20:04:50 -0700

    added database project

---
+++ Database/LibraryDB/StoredProcedures/DeassociateBooksFromAuthors.sql
@@ -0,0 +1,21 @@
+create procedure dbo.stp_deassociate_book_from_authors
+@bookId nvarchar(100),
+@authorsIds nvarchar(100)
+as
+with cte as
+(
+    select 
+        trim([value]) as author_id,
+        @bookId as book_id    
+    from
+        string_split(@authorsIds, ',')
+)
+delete 
+    ba
+from
+    dbo.books_authors ba
+inner join
+    cte c on ba.author_id = c.author_id and ba.book_id = c.book_id
+
+select book_id, author_id from dbo.books_authors where book_id = @bookId
+go
